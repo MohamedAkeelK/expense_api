f579309a80221f42e65fc5241f3b2b1a21fb8710
Mohamed Akeel Khan <Mohamedakeelkhan@gmail.com>
2024-12-10 18:07:51 -0500

    added user model and register route/functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("hello from auth");
+// REGISTER
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({
+    username: username,
+    email: email,
+    password: password,
+  });
+
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
