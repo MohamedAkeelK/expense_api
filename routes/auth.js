b1d661a72380e3e8772df22ee505d813c7b8cde4
Mohamed Akeel Khan <Mohamedakeelkhan@gmail.com>
2024-12-10 21:45:51 -0500

    configured bcrpt to hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // REGISTER
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({
-    username: username,
-    email: email,
-    password: password,
-  });
 
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: hashedPassword,
+    });
+
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
