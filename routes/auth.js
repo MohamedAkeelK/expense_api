c58159a721fc657030851324764e5999d8e1a615
Mohamed Akeel Khan <Mohamedakeelkhan@gmail.com>
2024-12-11 23:37:24 -0500

    added verify function

--- routes/auth.js
+++ routes/auth.js
@@ -55,4 +55,23 @@ router.post("/login", async (req, res) => {
   }
 });
 
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, "mySecretKey", (err, user) => {
+      if (err) {
+        return res.status(403).json("Token is not valid");
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json("You are not authenticated");
+  }
+};
+
+// router.delete("/")
+
 module.exports = router;
